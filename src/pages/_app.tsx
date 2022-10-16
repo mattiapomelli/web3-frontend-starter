ebc21c046aeaa145b132b0cb2e68cf69c8651ca2
mattiapomelli <mattiapomelli@gmail.com>
2022-10-16 17:07:56 +0200

    Add walletConnect connector

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import "../styles/globals.css";
 import { DefaultSeo } from "next-seo";
 import { ThemeProvider } from "next-themes";
 import { configureChains, chain, createClient, WagmiConfig } from "wagmi";
+import { InjectedConnector } from "wagmi/connectors/injected";
+import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
 
@@ -12,7 +14,7 @@ import SEO from "../../next-seo.config";
 
 import type { AppProps } from "next/app";
 
-const { provider, webSocketProvider } = configureChains(
+const { chains, provider, webSocketProvider } = configureChains(
   [chain.mainnet],
   [
     alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY }),
@@ -20,8 +22,24 @@ const { provider, webSocketProvider } = configureChains(
   ],
 );
 
+const connectors = [
+  new InjectedConnector({
+    chains,
+  }),
+  new WalletConnectConnector({
+    chains,
+    options: {
+      rpc: {
+        [chain.mainnet
+          .id]: `https://eth-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`,
+      },
+    },
+  }),
+];
+
 const client = createClient({
   autoConnect: true,
+  connectors,
   provider,
   webSocketProvider,
 });

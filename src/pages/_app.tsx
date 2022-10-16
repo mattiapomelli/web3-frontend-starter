277212405c7fd9a099f3f58d44548bf338a2c715
mattiapomelli <mattiapomelli@gmail.com>
2022-10-16 16:56:59 +0200

    Add alchemy provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import { DefaultSeo } from "next-seo";
 import { ThemeProvider } from "next-themes";
 import { configureChains, chain, createClient, WagmiConfig } from "wagmi";
+import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
 
 import { DefaultLayout } from "@layouts/default-layout";
@@ -13,7 +14,10 @@ import type { AppProps } from "next/app";
 
 const { provider, webSocketProvider } = configureChains(
   [chain.mainnet],
-  [publicProvider()],
+  [
+    alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY }),
+    publicProvider(),
+  ],
 );
 
 const client = createClient({

dfc6910ea4fd9e66a88ae8e93668ddc893b75596
mattiapomelli <mattiapomelli@gmail.com>
2022-10-22 19:12:55 +0200

    Add contract address and helper hook

---
+++ src/hooks/use-storage-contract-params.ts
@@ -0,0 +1,15 @@
+import { useNetwork } from "wagmi";
+
+import { StorageAbi } from "@abis/Storage";
+import { STORAGE_ADDRESS } from "@constants/addresses";
+import { CHAIN } from "@constants/chains";
+
+export const useStorageContractParams = () => {
+  const { chain } = useNetwork();
+  const currentChain = chain ?? CHAIN;
+
+  return {
+    abi: StorageAbi,
+    address: STORAGE_ADDRESS[currentChain.id],
+  };
+};

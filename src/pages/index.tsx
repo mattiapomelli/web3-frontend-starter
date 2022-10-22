a5827d83034a94ae4541456ce25573e416d6b4a5
mattiapomelli <mattiapomelli@gmail.com>
2022-10-22 19:14:30 +0200

    Set wagmi persister to null

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,22 @@
 import React from "react";
+import { useContractRead } from "wagmi";
+
+import { useStorageContractParams } from "@hooks/use-storage-contract-params";
 
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div>Home</div>;
+  const params = useStorageContractParams();
+  const { data } = useContractRead({
+    ...params,
+    functionName: "getData",
+  });
+
+  return (
+    <div>
+      <div>{data}</div>
+    </div>
+  );
 };
 
 export default Home;
